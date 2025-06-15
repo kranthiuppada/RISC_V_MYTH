c7a9d6cde3362a08d2b00969191c7b16385207d9
KRANTHI UPPADA <kranthi.uppada1234@gmail.com>
2025-06-15 22:07:36 +0530

    Update sm1ton.c

--- LAB_WORK/Day_1/sm1ton.c
+++ LAB_WORK/Day_1/sm1ton.c
@@ -1,6 +1,5 @@
 //C program from 1to n in virtualbox
-vsduser@vsduser-VirtualBox:~$ cd
-vsduser@vsduser-VirtualBox:~$ leafpad sm1ton.c 
+
 #include  <stdio.h>
 
 int main(){
@@ -11,9 +10,9 @@ int main(){
 	printf("Sum of numbers from 1 to %d is %d\n",n, sum);
 	return 0;
 }
-//For compiling 
-vsduser@vsduser-VirtualBox:~$ gcc sm1ton.c
-//for output
-vsduser@vsduser-VirtualBox:~$ ./a.out
+//compile by command
+gcc sm1ton.c
+// output by command
+ ./a.out
 sum of numbers 1 to 5 is 15
 	
