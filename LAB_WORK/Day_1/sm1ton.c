792c23b09c020545d4aedfcaa930a18f2e7073cb
kranthiuppada <kranthi.uppada123@gmail.com>
2025-06-28 23:38:02 +0530

    Add sm1ton.c to DAY_1

--- LAB_WORK/Day_1/sm1ton.c
+++ LAB_WORK/Day_1/sm1ton.c
@@ -1,5 +1,3 @@
-//C program from 1to n in virtualbox
-
 #include  <stdio.h>
 
 int main(){
@@ -10,9 +8,3 @@ int main(){
 	printf("Sum of numbers from 1 to %d is %d\n",n, sum);
 	return 0;
 }
-//compile by command
-gcc sm1ton.c
-// output by command
- ./a.out
-sum of numbers 1 to 5 is 15
-	
